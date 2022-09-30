9fa5861d7cfbc9b160f068e38497e488125135a4
A.Brian Lopez <bl@flexiopagos.com>
2022-09-30 17:48:23 -0300

    js2 primeros ejercicios

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -225,9 +225,10 @@ return  base * altura /2
 function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
-  //Escribe tu código aquí
-  return 
-}return 1 * 1.20 
+  //Escribe tu código aquí 
+  
+  return 1 * 1.20
+} 
 
 
 function esVocal(letra){
